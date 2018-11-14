f8ee7fdda75264df5ebd3c6df443da35ea34804a
Falanke21 <1065314944@qq.com>
2018-11-14 01:11:11 -0500

    add fsm and datapath into v

--- Lab7/part2/lab7_part2/lab7_part2/part2.v
+++ Lab7/part2/lab7_part2/lab7_part2/part2.v
@@ -31,10 +31,10 @@ module part2
 	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
 	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
 	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
-	
+
 	wire resetn;
 	assign resetn = KEY[0];
-	
+
 	// Create the colour, x, y and writeEn wires that are inputs to the controller.
 	wire [2:0] colour;
 	wire [7:0] x;
@@ -64,14 +64,96 @@ module part2
 		defparam VGA.MONOCHROME = "FALSE";
 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
 		defparam VGA.BACKGROUND_IMAGE = "black.mif";
-			
+
 	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
 	// for the VGA controller, in addition to any other functionality your design may require.
-    
+
     // Instansiate datapath
 	// datapath d0(...);
 
     // Instansiate FSM control
     // control c0(...);
-    
+
 endmodule
+
+module fsm (clock, resetn, go, x_enable, ld_x, ld_y, ld_color, writeEn);
+	input clk, resetn, go, x_enable;
+	output reg ld_x, ld_y, ld_color, writeEn;
+
+	reg [1:0] current_state, next_state;
+
+	localparam  S_LOAD        = 2'd0,
+							S_LOAD_WAIT   = 2'd1,
+							S_LOAD_Y			= 2'd2,
+							S_PLOT        = 2'd3;
+
+	always @ ( * ) begin
+		case (current_state)
+			S_LOAD_X: next_state = x_enable ? S_LOAD_X_WAIT : S_LOAD_X;
+			S_LOAD_X_WAIT: next_state = x_enable ? S_LOAD_X_WAIT : S_LOAD_Y;
+			S_LOAD_Y: next_state = go ? S_PLOT : S_LOAD_Y;
+			S_PLOT: next_state = go ? S_PLOT : S_LOAD_X;
+			default: next_state = S_LOAD_X;
+		endcase
+	end
+
+	always @ ( * ) begin
+		ld_x = 0;
+		ld_color = 0;
+	 	ld_y = 0;
+		writeEn = 0;
+
+		case (current_state)
+			S_LOAD_X: begin
+				ld_x = 1'b1;
+			end
+			S_LOAD_Y: begin
+				ld_y = 1'b1;
+				ld_color = 1'b1;
+			end
+			S_PLOT: begin
+				writeEn: 1'b1;
+			end
+
+		endcase
+	end
+
+	// current_state registers
+	always@(posedge clock)
+	begin:
+			if(!resetn)
+					current_state <= S_LOAD_X;
+			else
+					current_state <= next_state;
+	end // state_FFS
+
+endmodule // fsm
+
+module datapath (clock, data_in, ld_x, ld_y, ld_color, color_in, resetn,
+								x_out, y_out, color_out);
+	input clock, ld_x, resetn, ld_x, ld_y, ld_color;
+	input [6:0] data_in;
+	input [2:0] color_in;
+	output reg [7:0] x_out;
+	output reg [6:0] y_out;
+	output reg [2:0] color_out;
+
+	always @ ( posedge clock) begin
+		if (!resetn) begin
+			x_out <= 8'd0;
+			y_out <= 7'd0;
+			color_out <= 3'd0;
+		end
+		else begin
+			if (ld_x)
+				x_out <= {0, data_in};
+			if (ld_y)
+				y_out <= data_in;
+			if (ld_color)
+				color_out <= color_in;
+		end
+	end
+
+	//TODO ADD COUNTER 
+
+endmodule // datapath
